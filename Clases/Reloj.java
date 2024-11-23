6100163422673598bcf903b5f8b451a2cdb91a46
Ulises <ulisesoliva17@gmail.com>
2024-11-23 19:31:19 -0300

    Arreglo Reloj

--- Clases/Reloj.java
+++ Clases/Reloj.java
@@ -1,5 +1,4 @@
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.logging.Handler;
 
 public class Reloj implements Runnable{
     private static final int hora_apuertura = 9; 
@@ -22,19 +21,19 @@ public class Reloj implements Runnable{
                 //System.out.println(PrintColor.ANSI_YELLOW + "[CLASE RELOJ] Hora actual: " + Handler.formatoHora(hora.get()) + " hs" + PrintColor.ANSI_RESET);
 
                 switch (hora_actual.get()) {
-                    case hora_apuertura {
+                    case hora_apuertura: {
                         //aeropuerto.comenzarHorarioAtencion();
                         //Thread.sleep(1000);
                     }
-                    case hora_cierra {
+                    case hora_cierra: {
                         //aeropuerto.terminarHorarioAtencion();
                         //Thread.sleep(1000);
                     }
-                    case nuevo_dia {
+                    case nuevo_dia: {
                         hora_actual.set(0);
                         //System.out.println( PrintColor.ANSI_YELLOW + "[CLASE RELOJ] Comienza un nuevo día" + PrintColor.ANSI_RESET);
                     }
-                    default {
+                    default :{
                     }
                 }
 
