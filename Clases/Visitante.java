c510d5fcafec3fa79e8df6a7133ecbd8ce8274bb
MatiasDegli <matias.degli@gmail.com>
2024-11-29 16:59:11 -0300

    Test

--- Clases/Visitante.java
+++ Clases/Visitante.java
@@ -6,10 +6,10 @@ public class Visitante implements Runnable {
     private int puntos;
     private ControlTren tren;
     private Parque parque;
-
-    public Visitante(int idVisitante,Parque par, Comedor com, AreaJuegos area, ControlTren tr) {
+    
+    public Visitante(int idVisitante, Parque par, Comedor com, AreaJuegos area, ControlTren tr) {
         this.id = idVisitante;
-        parque=par;
+        parque = par;
         this.comedor = com;
         this.juegos = area;
         this.puntos = 0;
@@ -34,29 +34,27 @@ public class Visitante implements Runnable {
 
         try {
 
-
-            //tren.abordarTren(nombre);
-            parque.ingresarParque(this,id);
+            tren.abordarTren(nombre);
+            parque.ingresarParque(this, id);
             comedor.llegaVisitante(id);
-          num=comedor.buscaMesa(id);
-          
-          if(num>=0){
-          this.comiendo();
-          comedor.dejaMesa(num, id);
-          }
-          
-          comedor.saleVisitante(id);
+            num = comedor.buscaMesa(id);
 
-          // Intercambiar ficha y jugar
-         juegos.intercambiarFicha(id);
-          // Obtener puntos del juego
-          this.puntos = juegos.jugar(id);
-          // Recibir premio basado en los puntos
-          juegos.recibirPremio(puntos,id);
-         
-    
+            if (num >= 0) {
+                this.comiendo();
+                comedor.dejaMesa(num, id);
+            }
+
+            comedor.saleVisitante(id);
+
+            // Intercambiar ficha y jugar
+            juegos.intercambiarFicha(id);
+            // Obtener puntos del juego
+            this.puntos = juegos.jugar(id);
+            // Recibir premio basado en los puntos
+            juegos.recibirPremio(puntos, id);
+
         } catch (Exception e) {
         }
-        
-}
+
+    }
 }
